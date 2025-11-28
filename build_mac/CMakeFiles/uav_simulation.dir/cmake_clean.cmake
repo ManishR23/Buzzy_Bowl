b708cec70f1e321a5291c99ec35c355c74dc37b4
Kevin Ghobrial <kghobrial3@gatech.edu>
2025-11-28 15:44:54 -0500

    fixed everything

---
+++ build_mac/CMakeFiles/uav_simulation.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uav_simulation.dir/ECE_UAV.cpp.o"
+  "CMakeFiles/uav_simulation.dir/ECE_UAV.cpp.o.d"
+  "CMakeFiles/uav_simulation.dir/ObjModel.cpp.o"
+  "CMakeFiles/uav_simulation.dir/ObjModel.cpp.o.d"
+  "CMakeFiles/uav_simulation.dir/main.cpp.o"
+  "CMakeFiles/uav_simulation.dir/main.cpp.o.d"
+  "uav_simulation"
+  "uav_simulation.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uav_simulation.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
